a11c3c67f370dbd8072d8479126187a8be740985
hoonlucky7 <hoon.lucky7@gmail.com>
2019-09-20 20:17:08 +0900

    PrecisionTest

---
+++ javaWorkspace/PrecisionTest/PrecisionTest.java
@@ -0,0 +1,9 @@
+class PrecisionTest {
+    public static void main(String[] args) {
+        float f = 1.234567890123456789f;
+        double d = 1.2345678901234567890;
+
+        System.out.println(f);
+        System.out.println(d);
+    } 
+}
